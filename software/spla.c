5843dcc0af5287cbc0172c2a8907a11b36d7934c
Chris Evans <khaydarian@gmail.com>
2020-04-18 12:21:17 -0700

    software: Add test_ftdi_toggle.

--- software/spla.c
+++ software/spla.c
@@ -1,6 +1,7 @@
 // vi: ts=2:sw=2:sts=2:noet
 #include <stdio.h>
 #include <stdbool.h>
+#include <unistd.h>
 #include <string.h>
 #include <ftdi.h>
 #include <libusb.h>
@@ -115,6 +116,61 @@ static int show_ftdi(int argc, char** argv) {
 	return 0;
 }
 
+#define FTDI_VENDOR_ID 0x0403
+#define FTDI_PRODUCT_ID 0x6014
+
+int test_ftdi_toggle(int argc, char** argv) {
+	// TODO handle per-command arguments
+	(void) argc;
+	(void) argv;
+
+	struct ftdi_context* ftdi = ftdi_new();
+	if (!ftdi) {
+		fprintf(stderr, "Error opening FTDI context.\n");
+		return 1;
+	}
+
+	int ret = ftdi_usb_open_desc(ftdi, FTDI_VENDOR_ID, FTDI_PRODUCT_ID,
+			"C232HM-DDHSL-0", "FT0J7C2U");
+	if (ret) {
+		show_ftdi_error("ftdi_usb_open_desc", ftdi, ret);
+		ftdi_free(ftdi);
+		return 1;
+	}
+
+	ret = ftdi_set_bitmode(ftdi, 0xFF, BITMODE_BITBANG);
+	if (ret) {
+		show_ftdi_error("ftdi_set_bitmode", ftdi, ret);
+	} else {
+		unsigned char buf[1];
+
+		for (int i = 0; i < 10; i++) {
+			printf("On\n");
+			buf[0] = 0xFF;
+			ret = ftdi_write_data(ftdi, buf, 1);
+			if (ret < 0) {
+				show_ftdi_error("ftdi_write_data", ftdi, ret);
+			}
+			usleep(1000000);
+			printf("Off\n");
+			buf[0] = 0x0;
+			ret = ftdi_write_data(ftdi, buf, 1);
+			if (ret < 0) {
+				show_ftdi_error("ftdi_write_data", ftdi, ret);
+			}
+			usleep(1000000);
+		}
+	}
+
+	ret = ftdi_usb_close(ftdi);
+	if (ret) {
+		show_ftdi_error("ftdi_usb_close", ftdi, ret);
+	}
+
+	ftdi_free(ftdi);
+	return 0;
+}
+
 int global_flag(int argc, char** argv) {
 	// TODO parse global flags here, and not just skip over them
 	(void) argc;
@@ -130,6 +186,10 @@ struct command all_commands[] = {
 	{
 		"show_ftdi", show_ftdi,
 		"Show all relevant devices connected by USB.",
+	},
+	{
+		"test_ftdi_toggle", test_ftdi_toggle,
+		"Test FTDI interface by toggling GPIOs.",
 	},
 	{0, 0, 0},
 };
