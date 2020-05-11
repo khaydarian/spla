1903871dc90ba7ce8a3848f9db0ed57846bf342b
Chris Evans <khaydarian@gmail.com>
2020-05-10 17:18:06 -0700

    software: Modify ftdi_test_spi to add frequency flag and repeat option.

--- software/ftdi_test_spi.c
+++ software/ftdi_test_spi.c
@@ -8,15 +8,45 @@
 
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
+#include <signal.h>
+
+bool flag_repeat = false;
+char* flag_freq = "1MHz";
+
+static status flag(int argc, char** argv, int* absorbed) {
+	if (!strcmp(argv[0], "--freq")) {
+		if (argc == 1) {
+			return errorf("Missing required argument for flag --freq.");
+		}
+		flag_freq = argv[1];
+		*absorbed = 2;
+		return OK;
+	}
+	if (!strcmp(argv[0], "--repeat")) {
+		flag_repeat = true;
+		*absorbed = 1;
+		return OK;
+	}
+	return errorf("Unexpected argument: %s", argv[0]);
+}
+
+static void sigint_handler(int signum) {
+	(void) signum;
+	flag_repeat = false;
+}
 
 // [Command]
 // Description: Test FTDI SPI interface.
 // Option: open_usb = true
 // Option: default_usb_device = cable
 status ftdi_test_spi(int argc, char** argv) {
-	// TODO handle per-command arguments
-	(void) argc;
-	(void) argv;
+	while (argc) {
+		int absorbed;
+		RETURN_IF_ERROR(flag(argc, argv, &absorbed));
+		argc -= absorbed;
+		argv += absorbed;
+	}
 
 	status err = OK;
 
@@ -25,25 +55,31 @@ status ftdi_test_spi(int argc, char** argv) {
 		goto bad;
 	}
 
-	err = mpsse_set_frequency("1MHz");
+	err = mpsse_set_frequency(flag_freq);
 	if (is_error(err)) {
 		goto bad;
 	}
-
-	mpsse_chip_select(true);
 
 	unsigned char data[255];
 	for (unsigned i = 0; i < sizeof(data); i++) {
 		data[i] = i;
 	}
 
-	mpsse_write_data(data, sizeof(data));
+	if (flag_repeat) {
+		signal(SIGINT, sigint_handler);
+	}
 
-	mpsse_chip_select(false);
+	while (flag_repeat) {
+		mpsse_chip_select(true);
 
-	int ret = ftdiutil_flush_data(ftdi);
-	if (ret) {
-		err = ftdiutil_error("ftdiutil_flush_data", ret);
+		mpsse_write_data(data, sizeof(data));
+
+		mpsse_chip_select(false);
+
+		int ret = ftdiutil_flush_data(ftdi);
+		if (ret) {
+			err = ftdiutil_error("ftdiutil_flush_data", ret);
+		}
 	}
 
 bad:
