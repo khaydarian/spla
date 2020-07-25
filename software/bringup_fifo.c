a55dbda93631e1ffc968c348d1f7304d7692257d
Chris Evans <khaydarian@gmail.com>
2020-07-25 13:31:03 -0700

    software: Add explicit pass/fail to bringup_fifo.

--- software/bringup_fifo.c
+++ software/bringup_fifo.c
@@ -22,6 +22,41 @@ static void print_bits(unsigned char b, unsigned char mask) {
   }
 }
 
+static int bits_set(uint16_t x) {
+  static int nibble[] = {0, 1, 1, 2, 1, 2, 2, 3, 1, 2, 2, 3, 2, 3, 3, 4};
+  return (nibble[(x >> 0) & 0xf] + nibble[(x >> 4) & 0xf] +
+          nibble[(x >> 8) & 0xf] + nibble[(x >> 12) & 0xf]);
+}
+
+static bool pattern_good(uint16_t pattern) {
+  pattern &= 0xefff;  // Ignore NC pin.
+  return bits_set(pattern) == 1;
+}
+
+static uint32_t patterns_found[65536 / 32] = {0};
+static int patterns_good = 0;
+static int patterns_bad = 0;
+
+static void found_pattern(uint16_t pattern) {
+  int word = pattern / 32;
+  int bit = 1 << (pattern % 32);
+  if (patterns_found[word] & bit) {
+    return;
+  }
+  patterns_found[word] |= bit;
+  if (pattern_good(pattern)) {
+    patterns_good++;
+  } else {
+    patterns_bad++;
+  }
+}
+
+#define PASS_GOOD_PATTERNS 15
+
+#define GREEN "[92m"
+#define RED "[91m"
+#define RESET "[0m"
+
 // [Command]
 // Description: Test FTDI FIFO pins.
 // Option: open_usb = true
@@ -51,6 +86,18 @@ status bringup_fifo(int argc, char** argv) {
       print_bits(high, 0xef);
       printf(" low ");
       print_bits(low, 0xff);
+
+      uint16_t pattern = (high << 8) | low;
+      found_pattern(pattern);
+      printf(" good %2d / %2d, bad %d ", patterns_good, PASS_GOOD_PATTERNS,
+             patterns_bad);
+      if (patterns_bad) {
+        printf(RED "FAIL" RESET);
+      } else if (patterns_good == PASS_GOOD_PATTERNS) {
+        printf(GREEN "PASS" RESET);
+      } else {
+        printf("----");
+      }
       printf("\n");
       last_low = low;
       last_high = high;
