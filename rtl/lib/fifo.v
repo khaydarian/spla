bf7b6a8ca52a64f50c0d13112846da1a5f877197
Chris Evans <khaydarian@gmail.com>
2020-08-01 15:12:11 -0700

    rtl: Add fifo to test_uart, with cork operation.

---
+++ rtl/lib/fifo.v
@@ -0,0 +1,58 @@
+// vim: set ts=4:sts=4:sw=4:noet
+`default_nettype none
+
+module fifo(
+	input  clock,
+	input  reset,
+	input  write_i,
+	input  [DATA_BITS-1:0] write_data_i,
+	output write_ready_o,
+	input  read_i,
+	output [DATA_BITS-1:0] read_data_o,
+	output read_ready_o);
+
+parameter DATA_BITS = 8;
+parameter DEPTH_BITS = 2;
+
+localparam MEMORY_SIZE = (1<<DEPTH_BITS);
+
+reg [DEPTH_BITS:0] read_addr;
+reg [DEPTH_BITS:0] write_addr;
+
+initial read_addr = 0;
+initial write_addr = 0;
+
+wire empty = (read_addr == write_addr);
+wire full = (
+	read_addr[DEPTH_BITS] != write_addr[DEPTH_BITS] &&
+	read_addr[DEPTH_BITS-1:0] == write_addr[DEPTH_BITS-1:0]);
+
+assign read_ready_o = ~empty;
+assign write_ready_o = ~full;
+
+wire do_read;
+wire do_write;
+assign do_read = read_i & ~empty;
+assign do_write = write_i & ~full;
+
+reg [DATA_BITS-1:0] memory [0:MEMORY_SIZE-1];
+
+always @(posedge clock)
+	if (reset)
+		read_addr <= 0;
+	else if (do_read)
+		read_addr <= read_addr + 1;
+
+assign read_data_o =
+	(do_read && do_write && empty)
+	? write_data_i : memory[read_addr[DEPTH_BITS-1:0]];
+
+always @(posedge clock)
+	if (reset)
+		write_addr <= 0;
+	else if (do_write) begin
+		write_addr <= write_addr + 1;
+		memory[write_addr[DEPTH_BITS-1:0]] <= write_data_i;
+	end
+
+endmodule
--- rtl/test_uart.v
+++ rtl/test_uart.v
@@ -10,6 +10,22 @@ module test_uart(
 
 wire [7:0] incoming_data;
 wire incoming_valid;
+wire [7:0] outgoing_data;
+wire fifo_has_space;
+wire fifo_available;
+wire tx_busy;
+
+reg fifo_corked;
+always @(posedge clock)
+	if (incoming_valid) begin
+		if (incoming_data == "e")
+			fifo_corked <= 1;
+		if (incoming_data == "z")
+			fifo_corked <= 0;
+	end
+
+wire do_tx;
+assign do_tx = fifo_available && !tx_busy && !fifo_corked;
 
 uart_rx #(.CLOCKS_PER_BAUD(104)) // 115200 baud
 	uart_rx0(
@@ -18,17 +34,29 @@ uart_rx #(.CLOCKS_PER_BAUD(104)) // 115200 baud
 		.valid_o(incoming_valid),
 		.rx_i(uart_rx));
 
+fifo #(.DEPTH_BITS(5))  // 32 elements
+	uart_fifo(
+		.clock(clock),
+		.reset(0),
+		.write_i(incoming_valid),
+		.write_data_i(incoming_data),
+		.write_ready_o(fifo_has_space),
+		.read_i(do_tx),
+		.read_data_o(outgoing_data),
+		.read_ready_o(fifo_available));
+
 uart_tx #(.CLOCKS_PER_BAUD(104)) // 115200 baud
 	uart_tx0(
 		.clock(clock),
-		.write_i(incoming_valid),
-		.data_i(incoming_data),
+		.write_i(do_tx),
+		.data_i(outgoing_data),
+		.busy_o(tx_busy),
 		.tx_o(uart_tx));
 
 oneshot #(.CYCLES(600000)) // 50ms
 	oneshot_led7(.clock(clock), .in(~uart_rx), .out(led7));
 
 oneshot #(.CYCLES(600000)) // 50ms
-	oneshot_led8(.clock(clock), .in(incoming_valid), .out(led8));
+	oneshot_led8(.clock(clock), .in(do_tx), .out(led8));
 
 endmodule
