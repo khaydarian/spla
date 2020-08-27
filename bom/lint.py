fd9e451f742696a1d133eafe0ecd50be1ef664ba
Chris Evans <khaydarian@gmail.com>
2020-08-26 21:05:28 -0700

    bom: Update manufacture.py for new board.

--- bom/lint.py
+++ bom/lint.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 # vi: ts=4:sw=4:sts=4:et
 
-import sys, argparse
+import sys
+import argparse
 import bom
 
-def main(args):
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument(
             '--bom', type=str, default=None,
@@ -18,4 +19,4 @@ def main(args):
         print('ok (%d components)' % len(b.components))
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main()
--- bom/manufacture.py
+++ bom/manufacture.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 # vi: ts=4:sw=4:sts=4:et
 
-import sys, os.path, bom
+import sys
+import os.path
+import argparse
+import bom
 ACCOUNTING_DIR = os.path.join(os.path.dirname(sys.argv[0]), '../accounting')
 sys.path.append(ACCOUNTING_DIR)
 import txtdb
@@ -18,6 +21,9 @@ class Stage(object):
         print("----- %s -----" % self.name)
         components_by_digikey = {}
         for r in self.refs:
+            if r not in c_by_ref:
+                # Probably a boring component
+                continue
             c = c_by_ref[r]
             if c.digikey not in components_by_digikey:
                 components_by_digikey[c.digikey] = []
@@ -26,10 +32,18 @@ class Stage(object):
         digikeys = list(components_by_digikey.keys())
         digikeys.sort(key=lambda d: refsortkey(components_by_digikey[d][0].ref))
         for digikey in digikeys:
+            if digikey == 'N/A':
+                continue
             components = components_by_digikey[digikey]
             print('[%-20s] %s%s%s (%r)' % (
-                digikey, GREEN, desc_by_digikey[digikey], RESET, components[0].value))
+                digikey, GREEN, desc_by_digikey[digikey], RESET, onlyvalue(components)))
             print(' ', ' '.join([c.ref for c in components]))
+
+def onlyvalue(vs):
+    for v in vs:
+        if v.value != vs[0].value:
+            return "various"
+    return vs[0].value
 
 def refsortkey(ref):
     if ref.startswith('R'):
@@ -39,25 +53,72 @@ def refsortkey(ref):
     return (0, ref)
 
 STAGES = [
-    Stage("FPGA-Only", "U1"),
-    Stage("Power", """
-        J7 D1 D2 L1 L2 R96 C92 C99 C94 R91 U8 C91 R98 C93 R97 R92 R86 C82 C89
-        C84 R81 U7 C81 R88 C83 R87 R82 C42 U14 R34 C41 U9 R33"""),
-    Stage("FTDI-Front", "U2 U3 C7 D3 D4 D5 D6 R12 R16 R8 R10 R9 R28"),
-    Stage("FPGA-Front", "D7 D8 R26 R27"),
-    # Skip R1 C1
-    Stage("FTDI-Back", "R13 R14 R2 R3 C16 R4 R5 R6 R7 C38 C4 C6 C3 C5 R31 X1 C2 C18 C43"),
-    Stage("FGPA-Back", "C31 C25 C13 R15 C30 C32 C37 C19 C24 C21 C35 C20 C15 C27 C33 C34 C23 C17"),
-    Stage("PinHeaders", "J12 J4 J2"),
-    Stage("LevelShifters-Back", """
-        U17 U16 U23 U22 U32 U30 U31 U20 U24 U25 U26 U27 U15 U28 U29
-        C12 C39 R17 R18 R19 R20 R21 R22 R23 R24 C22
-        R11 C11 C40 C29 C36 C26 R25 C8 C10 C9 C28 C14"""),
-    Stage("SNES-Front", "R29 R30"),
-    #Stage("Analog", ...),
+    Stage("Back:Stencil:FTDI", """
+C1 C2 C3 C4
+C14 C51 C52 C60 C64
+R1 R2 R4 R8
+R20 R25 R26 R27 R39 R40 R50 R51 R52 R53 R59
+R38 C67 C72
+X1
+       """),
+    Stage("Back:Stencil:FPGA", """
+C29 R6
+C11 C12 C25 C29 C32 C30
+C22 C27 C34 C26 C23 C28 C33 C31 C21 C24 C20
+            """),
+    Stage("Back:Stencil:Analog", """
+C107 C108 C151 C152 C153 C154
+C207 C208 C251 C252 C253 C254
+C307 C308 C351 C352 C353 C354
+C407 C408 C507 C508
+            """),
+    Stage("Back:Stencil:North","""
+U34 U33 C61 C65 C15 R76 C17 C8
+U32 U31 U30 C63 R5 C19 C49
+R30 R31 R32 R33 R34 R35 R36 R37
+            """),
+    Stage("Back:Stencil:South","""
+U29 U28 C66 C74 U27 R70 R71 R72 C79
+U26 C18 U25 C5 C16 U24 U23 U22 C62 C71
+C78 C73 U21 U20
+C6 C7
+            """),
+    Stage("Front:Stencil:Power", """
+D1 D2 R7 R28 L1 L2
+C92 C82 C99 C89 C94 C84 R91 R81 U8 U9
+C83 C93 R92 R82 C81 C91
+C9 C10 U6 U7 R73 R74
+            """),
+    Stage("Front:Stencil:FTDI", """
+U2 U3 R75 C53 D3 D4 D5 D6 U4 U5 R54
+            """),
+    Stage("Front:Stencil:FPGA", "U1"),
+    Stage("Front:Stencil:East", """
+R55 R56 R21 R22 R23 R24 D7 D8 D9 D10
+R58 C13 R57
+R123 R122 R121 R321 R322 R323
+R223 R222 R221
+           """),
+    Stage("Front:Stencil:Analog", """
+R111 C111 R211 C211 R311 C311
+R301 R302 C301 R309 R303 R304 U63 R308 C309 C53
+R201 R202 C201 R209 R203 R204 U62 R208 C209 C52
+R101 R102 C101 R109 R103 R104 U61 R108 C109 C51
+R408 U74 R508 U75 R502 R501 C501 C502 R402 R401 C401 C402
+R601 R602 R603 U80
+U51 U52 U53
+           """),
+    Stage("Manual:PinHeaders", "J2 J4 J7 J8 J5 J6 J9 J10 J11 J12"),
+    Stage("Manual:Misc", "J1 J3 SW1 SW2"),
+    Stage("Front:SNES", "U10 U11 U12 U13"),
 ]
 
-def main(args):
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+            '--bom', type=str, default=None,
+            help='BoM xml file.')
+    args = parser.parse_args()
 
     db = txtdb.Txtdb()
     db.parse_file(os.path.join(ACCOUNTING_DIR, 'board.txtdb'))
@@ -68,7 +129,7 @@ def main(args):
         assert desc not in desc_by_digikey or desc_by_digikey[digikey] == desc
         desc_by_digikey[digikey] = desc
 
-    b = bom.parse_xml(args)
+    b = bom.parse_xml(args.bom)
     b.remove_boring_components()
 
     c_by_ref = {}
@@ -77,5 +138,22 @@ def main(args):
     for stage in STAGES:
         stage.show(c_by_ref, desc_by_digikey)
 
+    staged_components = []
+    for stage in STAGES:
+        for ref in stage.refs:
+            staged_components.extend(stage.refs)
+
+    unstaged_components = []
+    for ref in c_by_ref.keys():
+        if ref.startswith('BOM'):
+            continue
+        if ref in staged_components:
+            continue
+        unstaged_components.append(ref)
+
+    if unstaged_components:
+        print('----- Missing! -----')
+        print(' '.join(sorted(unstaged_components)))
+
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    main()
